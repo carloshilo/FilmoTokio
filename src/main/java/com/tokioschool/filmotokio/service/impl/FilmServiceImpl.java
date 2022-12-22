cc4bc3c13218761b33907b5dd6460ce53ce3d2e4
carloshilo <carloshiloalas@gmail.com>
2022-12-22 21:20:04 +0100

    fix film service

--- src/main/java/com/tokioschool/filmotokio/service/impl/FilmServiceImpl.java
+++ src/main/java/com/tokioschool/filmotokio/service/impl/FilmServiceImpl.java
@@ -9,85 +9,79 @@ import com.tokioschool.filmotokio.service.FileService;
 import com.tokioschool.filmotokio.service.FilmService;
 import com.tokioschool.filmotokio.service.filmsearch.FilmSearch;
 import com.tokioschool.filmotokio.utils.StringUtil;
+import java.util.HashSet;
+import java.util.Set;
+import javax.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.transaction.Transactional;
-import java.util.Set;
-
 @Slf4j
 @Service
+@AllArgsConstructor
 public class FilmServiceImpl implements FilmService {
 
-    private final FilmRepository filmRepo;
-    private final FilmSearch filmSearch;
-    private final FileService fileService;
+  private final @NonNull FilmRepository filmRepository;
+  private final @NonNull FilmSearch filmSearch;
+  private final @NonNull FileService fileService;
 
-    public FilmServiceImpl(FilmRepository filmRepo,
-                           FilmSearch filmSearch, @Qualifier("film-poster-file-service")
-                           FileService fileService) {
-        this.filmRepo = filmRepo;
-        this.filmSearch = filmSearch;
-        this.fileService = fileService;
-    }
+  @Override
+  @Transactional
+  public Film addFilm(Film film) {
+    log.info("Saving Film {}", film);
+    return filmRepository.save(film);
+  }
 
-    @Override
-    @Transactional
-    public Film addFilm(Film film) {
-        film.setUri(StringUtil.getFilmUri(film.getTitle(), film.getYear()));
-        log.info("Saving Film {}", film);
-        return filmRepo.save(film);
-    }
+  @Override
+  @Transactional
+  public Film savePoster(Film film, MultipartFile posterImage) {
+    Film toUpdate = filmRepository.findByTitleIgnoreCase(film.getTitle())
+        .orElseThrow(() -> new FilmNotFoundException());
 
-    @Override
-    @Transactional
-    public Film savePoster(Film film, MultipartFile posterImage) {
-        Film toUpdate = filmRepo.findByTitleIgnoreCase(film.getTitle())
-                .orElseThrow(() -> new FilmNotFoundException());
+    String fileName = StringUtil.getFilmPosterFilename(film.getTitle(), film.getYear(),
+        posterImage.getContentType());
+    fileService.saveFile(posterImage, fileName);
 
-        String fileName = StringUtil.getFilmPosterFilename(film.getTitle(), film.getYear(), posterImage.getContentType());
-        fileService.saveFile(posterImage, fileName);
+    toUpdate.setPoster(fileName);
+    return filmRepository.save(toUpdate);
+  }
 
-        toUpdate.setPoster(fileName);
-        return filmRepo.save(toUpdate);
-    }
+  @Override
+  @Transactional()
+  public Film getFilmByUri(String filmUri) {
+    String filmTitle = StringUtil.getFilmTitleFromUri(filmUri);
+    return filmRepository.findByTitleIgnoreCase(filmTitle)
+        .orElseThrow(() -> new FilmNotFoundException());
+  }
 
-    @Override
-    @Transactional()
-    public Film getFilmByUri(String filmUri) {
-        String filmTitle = StringUtil.getFilmTitleFromUri(filmUri);
-        return filmRepo.findByTitleIgnoreCase(filmTitle)
-                .orElseThrow(() -> new FilmNotFoundException());
-    }
+  @Override
+  @Transactional
+  public Film addScore(String filmUri, Score score) {
+    Film toUpdate = getFilmByUri(filmUri);
+    toUpdate.getScores().add(score);
+    return filmRepository.save(toUpdate);
+  }
 
-    @Override
-    @Transactional
-    public Film addScore(String filmUri, Score score) {
-        Film toUpdate = getFilmByUri(filmUri);
-        toUpdate.addScore(score);
-        return filmRepo.save(toUpdate);
-    }
+  @Override
+  public Set<Film> searchByTitle(String title) {
+    log.info("Searching for Film with title like {}", title);
+    return filmRepository.findByTitleContainsIgnoreCase(title);
+  }
 
-    @Override
-    public Set<Film> searchByTitle(String title) {
-        log.info("Searching for Film with title like {}", title);
-        return filmRepo.findByTitleContainsIgnoreCase(title);
-    }
+  @Override
+  public Set<Film> getAll() {
+    return new HashSet<>(filmRepository.findAll());
+  }
 
-    @Override
-    public Set<Film> getAll() {
-        return filmRepo.findAll();
-    }
+  @Override
+  public Set<Film> searchFilms(String searchParam, String searchCriteria) {
+    return filmSearch.searchFilm(searchParam, FilmSearchCriteria.fromString(searchCriteria));
+  }
 
-    @Override
-    public Set<Film> searchFilms(String searchParam, String searchCriteria) {
-        return filmSearch.searchFilm(searchParam, FilmSearchCriteria.fromString(searchCriteria));
-    }
-
-    @Override
-    public Film findByTitleExact(String title) {
-        return filmRepo.findByTitleIgnoreCase(title).orElseThrow(FilmNotFoundException::new);
-    }
+  @Override
+  public Film findByTitleExact(String title) {
+    return filmRepository.findByTitleIgnoreCase(title).orElseThrow(FilmNotFoundException::new);
+  }
 }
