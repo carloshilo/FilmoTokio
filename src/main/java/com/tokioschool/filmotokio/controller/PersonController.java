c37b14e6f4c7aa87e1b8c2a4218336defec6665b
carloshilo <carloshiloalas@gmail.com>
2023-01-17 18:48:52 +0100

    crsf problem

--- src/main/java/com/tokioschool/filmotokio/controller/PersonController.java
+++ src/main/java/com/tokioschool/filmotokio/controller/PersonController.java
@@ -2,46 +2,49 @@ package com.tokioschool.filmotokio.controller;
 
 import com.tokioschool.filmotokio.domain.Person;
 import com.tokioschool.filmotokio.service.PersonService;
+import java.security.Principal;
+import javax.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.security.Principal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
 @RequestMapping("/person")
+@AllArgsConstructor
 public class PersonController {
 
-    private final PersonService service;
+  private final @NonNull PersonService service;
 
-    public PersonController(PersonService service) {
-        this.service = service;
-    }
+  @GetMapping("/add")
+  public String addPerson(@RequestParam(name = "person", required = false) String message,
+      Model model) {
+    model.addAttribute("person", new Person());
+    return "new-person";
+  }
 
-    @GetMapping("/add")
-    public String addPerson(@RequestParam(name = "person", required = false) String message,
-                            Model model) {
-        model.addAttribute("person", new Person());
-        return "new-person";
+  @PostMapping("/add")
+  public String addPerson(@ModelAttribute @Valid Person person,
+      BindingResult result,
+      Model model,
+      Principal principal) {
+    if (result.hasErrors()) {
+      log.error("Creation of Person {} failed because: {}", person,
+          result.getAllErrors().toArray());
+      model.addAttribute("person", person);
+      return "new-person";
+    } else {
+      log.info("User {} added Person {}", principal.getName(), person);
+      service.addPerson(person);
+      return "redirect:/person/add?person=created";
     }
-
-    @PostMapping("/add")
-    public String addPerson(@ModelAttribute("person") @Valid Person person,
-                            BindingResult result,
-                            Model model,
-                            Principal principal) {
-        if (result.hasErrors()) {
-            log.error("Creation of Person {} failed because: {}", person, result.getAllErrors().toArray());
-            return "new-person";
-        } else {
-            log.info("User {} added Person {}", principal.getName(), person);
-            service.addPerson(person);
-            return "redirect:/person/add?person=created";
-        }
-    }
+  }
 }
