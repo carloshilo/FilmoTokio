dabdc22460485afd289f11b5935686d0285893e3
carloshilo <carloshiloalas@gmail.com>
2023-03-10 15:21:58 +0100

    clean and fix

--- src/main/java/com/tokioschool/filmotokio/controller/UserController.java
+++ src/main/java/com/tokioschool/filmotokio/controller/UserController.java
@@ -1,31 +1,20 @@
 package com.tokioschool.filmotokio.controller;
 
-import com.tokioschool.filmotokio.domain.User;
-import com.tokioschool.filmotokio.domain.dto.ChangePasswordDTO;
 import com.tokioschool.filmotokio.domain.dto.CreateUserDTO;
-import com.tokioschool.filmotokio.domain.dto.EditUserDTO;
-import com.tokioschool.filmotokio.exception.UnauthorizedException;
 import com.tokioschool.filmotokio.service.RoleService;
 import com.tokioschool.filmotokio.service.UserService;
-import java.security.Principal;
-import java.util.Objects;
 import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @Slf4j
@@ -62,84 +51,5 @@ public class UserController {
     userService.create(createUserDTO);
     status.setComplete();
     return "redirect:/index";
-  }
-
-  @GetMapping("/profile")
-  public String profile(Model model, Principal principal) {
-    if (Objects.nonNull(principal)) {
-      var user = userService.getByUsernameOrThrow(principal.getName());
-      model.addAttribute("user", user);
-      return "/profile";
-    } else {
-      return "redirect:/index";
-    }
-  }
-
-  @GetMapping("/profile/films/{username}")
-  public String userFilms(ModelAndView modelAndView, @PathVariable("username") String username) {
-    User user = userService.getByUsernameOrThrow(username);
-    modelAndView.addObject("films", user.getFilms());
-    return "searched-film";
-  }
-
-  @PostMapping("/image")
-  public String userImage(@RequestParam("userImage") MultipartFile imageFile, Principal principal) {
-    log.info("Saving User {} userImage", principal.getName());
-    userService.saveImage(principal.getName(), imageFile);
-    return "redirect:/profile/edit";
-  }
-
-  @GetMapping("/profile/edit")
-  public String editProfile(Model model, Principal principal) {
-    User user = userService.getByUsernameOrThrow(principal.getName());
-    model.addAttribute("editUserDTO", new EditUserDTO(user));
-    model.addAttribute("changePasswordDTO", new ChangePasswordDTO());
-    return "edit-profile";
-  }
-
-  @PostMapping("/edit")
-  public String updateUser(@Valid @ModelAttribute("editUserDTO") EditUserDTO editUserDTO,
-      BindingResult result,
-      ModelAndView modelAndView,
-      Principal principal) {
-    if (result.hasErrors()) {
-      modelAndView.addObject("changePasswordDTO", new ChangePasswordDTO());
-      return "edit-profile";
-    } else {
-      String loggedInUsername = principal.getName();
-      log.info("Updating {} to {}", loggedInUsername, editUserDTO);
-      User updated = userService.update(loggedInUsername, editUserDTO.map());
-      modelAndView.addObject("user", updated);
-      return "redirect:/profile/";
-    }
-  }
-
-  @PostMapping("/change-password")
-  public String changePassword(
-      @Valid @ModelAttribute("changePasswordDTO") ChangePasswordDTO changePassword,
-      BindingResult result,
-      ModelAndView modelAndView,
-      Authentication authentication) {
-    if (result.hasErrors()) {
-      modelAndView.addObject("editUserDTO", new EditUserDTO((User) authentication.getPrincipal()));
-      modelAndView.addObject("error", true);
-      return "edit-profile";
-    }
-    log.info("Changing Password for {}", authentication.getName());
-    userService.changePassword(authentication.getName(), changePassword.getOldPassword(),
-        changePassword.getPassword());
-    return "redirect:/profile/edit";
-  }
-
-  @GetMapping("/profile/delete")
-  public String delete(ModelAndView modelAndView) {
-    return "delete-user";
-  }
-
-  @GetMapping("/delete")
-  public String deleteUser(Principal principal) throws UnauthorizedException {
-    log.info("Deleting User {}", principal.getName());
-    userService.delete(principal.getName());
-    return "redirect:/logout";
   }
 }
--- src/test/java/com/tokioschool/filmotokiobatch/FilmoTokioBatchApplicationTests.java
+++ src/test/java/com/tokioschool/filmotokiobatch/FilmoTokioBatchApplicationTests.java
@@ -1,19 +1,30 @@
 package com.tokioschool.filmotokiobatch;
 
 import com.tokioschool.filmotokiobatch.config.JobBatchConfiguration;
+import com.tokioschool.filmotokiobatch.listeners.MigrateFilmStartListener;
+import com.tokioschool.filmotokiobatch.listeners.MigrateFilmWriteListener;
 import org.junit.After;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@ComponentScan(basePackages = "com.tokioschool")
 @SpringBatchTest
+@SpringBootTest
 @EnableAutoConfiguration
-@ContextConfiguration(classes = {JobBatchConfiguration.class})
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {JobBatchConfiguration.class, MigrateFilmStartListener.class, MigrateFilmWriteListener.class})
 public class FilmoTokioBatchApplicationTests {
 
     @Autowired
@@ -37,6 +48,8 @@ public class FilmoTokioBatchApplicationTests {
         var jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
         var actualJobInstance = jobExecution.getJobInstance();
         var actualJobExitStatus = jobExecution.getExitStatus();
+
+        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
     }
 
 }
